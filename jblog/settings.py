5fd9045fa00ca01eaba842ab1fe70827c68a2d66
root <root@johnpham.me>
2015-01-01 02:32:59 -0500

    works okay

--- jblog/settings.py
+++ jblog/settings.py
@@ -24,7 +24,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['104.236.117.75','johnpham.me''127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['johnpham.me','104.236.117.75']
 
 
 # Application definition
